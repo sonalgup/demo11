e7e8439b0e5d7e56cdc3f1b833811c21164e7dc8
Varun <gpt.varun.24@gmail.com>
2021-06-20 17:45:50 +0530

    defibugp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-               sysosc[ojfp
+
+           
+
+swerb[oet
+
+
 	}
 
 }
